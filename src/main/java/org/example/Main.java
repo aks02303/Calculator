68330410c8b571cb6f66c17ac9a3b62b645480cc
aks02303 <akshaypandey0302@gmail.com>
2024-10-21 17:07:53 +0000

    Webhook check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int op, flag = 0;
